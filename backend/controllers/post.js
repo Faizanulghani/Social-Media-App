2542b370f1bbebb1e6f6aa1aa6c365422fac8c11
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-07-09 23:35:50 +0500

    user can update post

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -116,3 +116,35 @@ exports.getPostsOfFollowing = async (req, res) => {
     });
   }
 }
+
+exports.updateCaption = async (req,res) =>{
+  try {
+    const post = await Post.findById(req.params.id);
+    if(!post){
+      return res.status(404).json({
+        success: false,
+        message: "Post Not Found",
+      });
+    }
+
+      if(post.owner.toString() !== req.user._id.toString()){
+        return res.status(401).json({
+          success: false,
+          message: "Unauthorized",
+        });
+      }
+
+      post.caption = req.body.caption;
+      await post.save();
+      res.status(200).json({
+        success: true,
+        message: "Caption Updated",
+      });
+
+  } catch (error) {
+      res.status(400).json({
+        success: false,
+        error: error.message,
+      });
+  }
+}
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const { createPost, likeAndUnlikePost, deletePost } = require('../controllers/post')
-const { isAuthenticated } = require('../middleware/isAuthenticated')
+const express = require("express");
+const {
+  createPost,
+  likeAndUnlikePost,
+  deletePost,
+  updateCaption,
+} = require("../controllers/post");
+const { isAuthenticated } = require("../middleware/isAuthenticated");
 
-let router = express.Router()
+let router = express.Router();
 
-router.route("/post/upload").post(isAuthenticated,createPost)
-router.route("/post/:id").get(isAuthenticated,likeAndUnlikePost).delete(isAuthenticated,deletePost)
+router.route("/post/upload").post(isAuthenticated, createPost);
+router
+  .route("/post/:id")
+  .get(isAuthenticated, likeAndUnlikePost)
+  .put(isAuthenticated, updateCaption)
+  .delete(isAuthenticated, deletePost);
 
-module.exports = router
+module.exports = router;
